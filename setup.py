988ba82ecc3a43250a8dae52e5e1497e8ff8f889
Cold_Eyed <thelegendthreeplayer@gmail.com>
2024-08-01 18:41:02 +0300

    reworking setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from json import loads
 from setuptools import setup, find_packages
 
 setup(
-    **loads(open('info.json', 'r').read()),
+    name='NestedDictUnpack',
+    version='0.1.0',
+    description='A utility to unpack nested dictionaries',
+    author='me14nch01ic',
+    author_email='mindtraveller11@gmail.com',
+    url='https://github.com/me14nch01ic/NestedDictUnpack',
     packages=find_packages(),
-    setup_requires=['setuptools_scm'],
-    include_package_data=True,
-    long_description=open('README.md').read(),
+    install_requires=[],
+    dependency_links=[
+        'git+https://github.com/me14nch01ic/NestedDictUnpack.git#egg=NestedDictUnpack'
+    ],
+    classifiers=[],
+    python_requires='>=3.6',
 )
